93cd7cd86432129a027b26226d03c7d8e6fccf97
Danila Lutsai <dlutsai98@gmail.com>
2026-06-20 23:29:57 +0300

    sumTwoSmallestNumbers

--- exercise1.1-sumOfTwoLowestPositiveIntegers.js
+++ exercise1.1-sumOfTwoLowestPositiveIntegers.js
@@ -1,3 +1,7 @@
 function sumTwoSmallestNumbers(numbers) {
-    return numbers
+    numbers.sort((a, b) => a - b)
+    return numbers[0] + numbers[1]
 }
+
+console.log(sumTwoSmallestNumbers([10, 5, 42, 2]))
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
