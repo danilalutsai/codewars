3ee4332442cc1dd1b2fe3d21966a375d31055893
Danila Lutsai <dlutsai98@gmail.com>
2026-06-21 10:23:19 +0300

    categorizeNewMember

---
+++ exercise1.2-categorizeNewMember.js
@@ -0,0 +1,36 @@
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+//
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+//
+// Input
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+//
+// Output
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+// function openOrSenior(data) {
+//     return data.map(([age, handicap]) => {
+//         return age >= 55 && handicap > 7 ? "Senior" : "Open"
+//     })
+// }
+
+function openOrSenior(data) {
+    const result = []
+
+    for (const person of data) {
+        const age = person[0]
+        const handicap = person[1]
+    
+        if (age >= 55 && handicap > 7) {
+            result.push("Senior")
+        } else {
+            result.push("Open")
+        }
+    }
+    return result
+}
+
+console.log(openOrSenior(data = [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
