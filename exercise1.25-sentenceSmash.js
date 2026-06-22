b6deab3bd7233791a6f354568053db659cecddf9
Danila Lutsai <dlutsai98@gmail.com>
2026-06-22 20:33:58 +0300

    Sentence Smash

---
+++ exercise1.25-sentenceSmash.js
@@ -0,0 +1,18 @@
+// Write a function that takes an array of words and smashes them together into 
+// a sentence and returns the sentence. You can ignore any need to sanitize words 
+// or add punctuation, but you should add spaces between each word. 
+// Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+//
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+function smash(words) {
+    // let sentence = ""
+    // for (const word of words) {
+    //     sentence += word + " "
+    // }
+    // return sentence.trim()
+    return words.join(" ")
+}
+
+console.log(smash(['hello', 'world', 'this', 'is', 'great']))
