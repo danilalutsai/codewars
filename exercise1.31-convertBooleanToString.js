129bbb77395b223b2fc115fc4e33c38fe303fa48
Danila Lutsai <dlutsai98@gmail.com>
2026-06-23 11:18:40 +0300

    Convert boolean to String

---
+++ exercise1.31-convertBooleanToString.js
@@ -0,0 +1,9 @@
+// Implement a function which convert the given boolean value into its string representation.
+//
+// Note: Only valid inputs will be given.
+
+function booleanToString(str) {
+  return String(str)
+}
+
+console.log(booleanToString(false))
