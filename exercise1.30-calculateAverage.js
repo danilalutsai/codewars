a0ee8671edbce61899e10fad9d155578cb5f77f3
Danila Lutsai <dlutsai98@gmail.com>
2026-06-23 10:44:18 +0300

    Calculate Average of an Array

---
+++ exercise1.30-calculateAverage.js
@@ -0,0 +1,12 @@
+// Write a function which calculates the average of the numbers in a given array.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+  if (array === 0 || array.length === 0) {
+    return 0
+  } 
+  return array.reduce((arr, number) => arr + number) / array.length
+}
+
+console.log(findAverage([1, 2, 3]))
