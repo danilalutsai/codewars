3885eb14f67399e659382bcf3b5da1cb7fe486dd
Danila Lutsai <dlutsai98@gmail.com>
2026-06-26 16:19:37 +0300

    Find the unique number

--- exercise1.59-findTheUniqueNumber.js
+++ exercise1.59-findTheUniqueNumber.js
@@ -1,7 +1,13 @@
 function findUnique(array) {
-  const [a, b, c] = array
-  const common = a === b || a === c ? a : c
-  return array.find((number) => number !== common)
+  // const [a, b, c] = array
+  // const common = a === b || a === c ? a : c
+  // return array.find((number) => number !== common)
+  array.sort((a, b) => a - b)
+  if (array[0] === array[1]) {
+    return array.pop()
+  } else {
+    return array[0]
+  }
 }
 
 console.log(findUnique([1, 1, 1, 2, 1, 1]))
