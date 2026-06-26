d53d8bbd8c64a6584c47fdf00699b6ebc407a20f
Danila Lutsai <dlutsai98@gmail.com>
2026-06-26 15:27:46 +0300

    Equal sides of an array

---
+++ exercise1.58-equalSidesOfAnArray.js
@@ -0,0 +1,19 @@
+function findEvenIndex(array) {
+  let totalSum = array.reduce((sum, num) => sum + num, 0)
+  let leftSum = 0
+
+  for (let i = 0; i < array.length; i++) {
+    const rightSum = totalSum - leftSum - array[i]
+    // console.log(rightSum)
+    // console.log(leftSum)
+
+    if (rightSum === leftSum) 
+      return i 
+
+    leftSum += array[i]
+  }
+
+  return -1
+}
+
+console.log(findEvenIndex([20,10,-80,10,10,15,35]))
