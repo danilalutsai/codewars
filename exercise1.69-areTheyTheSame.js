d88eb821cda23573eed2c21464e4232008940727
Danila Lutsai <dlutsai98@gmail.com>
2026-06-27 13:20:55 +0300

    Are they the same?

--- exercise1.69-areTheyTheSame.js
+++ exercise1.69-areTheyTheSame.js
@@ -1,24 +1,15 @@
-function comparison(array1, array2) {
-  const sortedArray1 = array1.sort((a, b) => a - b)
-  const sortedArray2 = array2.sort((a, b) => a - b)
-
-  if (array1.length !== array2.length) {
+function comp(a, b) {
+  if (!a || !b || a.length !== b.length) {
     return false
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (sortedArray1[i] * sortedArray1[i] === sortedArray2[i]) {
-        return true
-      } else if (sortedArray2[i] * sortedArray2[i] === sortedArray1[i]) {
-        return true
-      } else {
-        return false
-      }
-    }
   }
-  return false
+
+  const squaredA = a.map(num => num * num).sort((a, b) => a - b)
+  const sortedB = b.sort((a, b) => a - b)
+
+  return squaredA.every((num, index) => num === sortedB[index])
 }
 
-console.log(comparison(
-  [121, 14641, 20736, 361, 25921, 361, 20736, 361],
+console.log(comp(
   [121, 144, 19, 161, 19, 144, 19, 11], 
+  [121, 14641, 20736, 361, 25921, 361, 20736, 361],
 ))
