ade4bd6205d5b04467997a00898b11238004d509
Danila Lutsai <dlutsai98@gmail.com>
2026-06-27 17:59:53 +0300

    Find the missing letter

---
+++ exercise1.74-findTheMissingLetter.js
@@ -0,0 +1,16 @@
+function findMissingLetter(array) {
+  let arrayNumbers = []
+  for (const char of array) {
+    if (char >= "A" && char <= "z") {
+      arrayNumbers.push(char.charCodeAt(0))
+      for (let i = 0; i < arrayNumbers.length - 1; i++) {
+        if (arrayNumbers[i + 1] !== arrayNumbers[i] + 1) {
+          return String.fromCharCode(arrayNumbers[i] + 1)
+        }
+      }
+    }
+  }
+  return false
+}
+
+console.log(findTheMissingLetter(["a", "b", "c", "e"]))
